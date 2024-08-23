170ee535772f10a673b0f5480c8d0a3c05ddaadd
Saulo Cerqueira <saulodecerqueira@gmail.com>
2024-08-22 23:49:55 -0300

    Iniciação do projeto, connectionString adicionada e criações da classe Categorias

---
+++ ControleEstoque/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ControleEstoque
+{
+    class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext() : base(nameOrConnectionString: "controle_estoque_bq") {}
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ControleEstoque/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque
+{
+    class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        [MaxLength(200)]
+        public string Nome {  get; set; }
+    }
+}
